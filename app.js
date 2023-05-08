1708d09432c881b1fc018f61a13f2236e6ea7dea
JohnnyMCR <jmcastillo991@gmail.com>
2023-05-08 18:45:37 -0400

    dotenv data

--- app.js
+++ app.js
@@ -7,3 +7,12 @@ const snacksController = require("./Controllers/SnacksController.js");
 app.use(express.json());
 app.use(cors());
 app.use(logger('dev'));
+
+app.get("/", (req, res) => {
+    res.send("Welcome to our App");
+})
+
+//404 page
+app.get("*", (req, res) => {
+    res.status(404).json({ error: "Page not found"});
+})
