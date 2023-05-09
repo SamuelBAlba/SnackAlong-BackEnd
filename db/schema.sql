34291d49d9d870fa6282ff2d9395cbb73920ff60
SamuelBAlba <brayanbalba24@gmail.com>
2023-05-08 21:38:36 -0400

    database created

---
+++ db/schema.sql
@@ -0,0 +1,19 @@
+DROP DATABASE IF EXISTS snacks_dev;
+
+CREATE DATABASE snacks_dev;
+
+\c snacks_dev;
+
+CREATE TABLE snacks (
+    id SERIAL PRIMARY KEY,
+    name TEXT NOT NULL,
+    type TEXT NOT NULL,
+    sugar INT NOT NULL,
+    protein INT NOT NULL,
+    fiber INT NOT NULL,
+    sodium INT NOT NULL,
+    calories BIGINT,
+    img VARCHAR
+);
+
+-- (name, type, sugar, protein, fiber, calories, img)
---
+++ db/seed.sql
@@ -0,0 +1,11 @@
+\c snacks_dev
+
+
+INSERT INTO snacks (name, type, sugar, protein, fiber, sodium, calories, img) VALUES
+('Oreos', 'Sweet', 23, 2, 0, 220, 270, 'https://upload.wikimedia.org/wikipedia/commons/thumb/1/1b/Oreo-Two-Cookies.png/1200px-Oreo-Two-Cookies.png'),
+('Sour Patch Kids', 'Sour', 24, 0, 0, 25, 110, 'https://ipcdn.freshop.com/resize?url=https://images.freshop.com/00070462098525/6c977e6381b72db04a4de7eefd49d79e_large.png&width=512&type=webp&quality=90'),
+('Chobani Greek Yogurt', 'Sour', 4, 16, 0, 65, 90, 'https://vendorcontentportal.s3.ap-southeast-1.amazonaws.com/product/005012876.jpg'),
+('Cheetos Flaming Hot', 'Salty', 0, 1, 1, 210, 170, 'https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcRt8xgNwtFHWu1fEED0JYDUFKpgv543v2Si2mNNCmjSC_KqpQuX'),
+('DunkAroos', 'Sweet', 20, 1, 1, 210, 170, 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR-tsWSD5ptUt4Q9LI7a_KFrUDoEhPeb8yfzLYzvIvdRk1aDlLK'),
+('SkinnyPop Original Popcorn', 'Salty', 0, 2, 3, 75, 150, 'https://cdn.shopify.com/s/files/1/0454/7562/7170/products/skinnyoriginal_0.5oz_front.png?v=1638563720' ),
+('Mangos', 'Sweet', 46, 3, 5, 3, 201, 'https://lh3.googleusercontent.com/0T0i0Y2SgRxyu3SbbMkLBDut_1cQdrfhRlLvRZCMz0CqFB_ngESar9KNqnVC001sftrPZK_ersfFj0DdNXSZHeoD5CaL-jLsdewEAUM');
